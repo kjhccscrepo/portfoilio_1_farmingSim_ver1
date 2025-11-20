17574f0454d529de40dc4f320002142638e9c2ea
kjhccscrepo <j_kist1@mail.harpercollege.edu>
2025-11-20 11:51:17 -0600

    added a to-do list in int main so I don't forget what I need to do later

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 
 #include "logic/cupid.hpp"
 
+// to-do list
+// fix shop, as its currently semi-functioning.
+// add tests for all the test files in farms_test
+
+
 int main() {
     cupid game;
     game.runGame();
